fe83e9e1f387a5ad95ff333fbb3e6b97a1a76d3e
czlowiekimadlo <czlowiekimadlo@gmail.com>
2024-01-27 17:19:54 +0100

    Hipnoza

--- scenes/tricks/tricks.gd
+++ scenes/tricks/tricks.gd
@@ -5,7 +5,8 @@ var tricks: Array = [
 	preload("res://scenes/tricks/bahleda.tscn").instantiate(),
 	preload("res://scenes/tricks/helicopter.tscn").instantiate(),
 	preload("res://scenes/tricks/loki.tscn").instantiate(),
-	preload("res://scenes/tricks/poo.tscn").instantiate()
+	preload("res://scenes/tricks/poo.tscn").instantiate(),
+	preload("res://scenes/tricks/hipnoza.tscn").instantiate()
 ]
 
 func get_random_trick() -> Node2D:
