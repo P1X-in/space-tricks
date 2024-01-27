5b737b53249d40485f6e109f94ed8a8b465ee5b4
czlowiekimadlo <czlowiekimadlo@gmail.com>
2024-01-27 16:27:23 +0100

    Bahleda i Koczkodan

--- scenes/tricks/tricks.gd
+++ scenes/tricks/tricks.gd
@@ -1,7 +1,8 @@
 class_name Tricks
 
 var tricks: Array = [
-	preload("res://scenes/tricks/tower.tscn").instantiate()
+	preload("res://scenes/tricks/tower.tscn").instantiate(),
+	preload("res://scenes/tricks/bahleda.tscn").instantiate()
 ]
 
 func get_random_trick() -> Node2D:
